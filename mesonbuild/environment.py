2d6cc078d22cad8f861539ca944ce141beb37d34
Havard Graff <havard.graff@gmail.com>
2017-12-18 01:00:27 +0100

    environment: don't crash when objc compiler is not found
    Instead throw a nice exception, like intended.

--- mesonbuild/environment.py
+++ mesonbuild/environment.py
@@ -610,19 +610,20 @@ class Environment:
                 p, out, err = Popen_safe(compiler + arg)
             except OSError as e:
                 popen_exceptions[' '.join(compiler + arg)] = e
-            version = search_version(out)
-            if 'Free Software Foundation' in out:
-                defines = self.get_gnu_compiler_defines(compiler)
-                if not defines:
-                    popen_exceptions[' '.join(compiler)] = 'no pre-processor defines'
-                    continue
-                gtype = self.get_gnu_compiler_type(defines)
-                version = self.get_gnu_version_from_defines(defines)
-                return GnuObjCCompiler(ccache + compiler, version, gtype, is_cross, exe_wrap, defines)
-            if out.startswith('Apple LLVM'):
-                return ClangObjCCompiler(ccache + compiler, version, CLANG_OSX, is_cross, exe_wrap)
-            if out.startswith('clang'):
-                return ClangObjCCompiler(ccache + compiler, version, CLANG_STANDARD, is_cross, exe_wrap)
+            else:
+                version = search_version(out)
+                if 'Free Software Foundation' in out:
+                    defines = self.get_gnu_compiler_defines(compiler)
+                    if not defines:
+                        popen_exceptions[' '.join(compiler)] = 'no pre-processor defines'
+                        continue
+                    gtype = self.get_gnu_compiler_type(defines)
+                    version = self.get_gnu_version_from_defines(defines)
+                    return GnuObjCCompiler(ccache + compiler, version, gtype, is_cross, exe_wrap, defines)
+                if out.startswith('Apple LLVM'):
+                    return ClangObjCCompiler(ccache + compiler, version, CLANG_OSX, is_cross, exe_wrap)
+                if out.startswith('clang'):
+                    return ClangObjCCompiler(ccache + compiler, version, CLANG_STANDARD, is_cross, exe_wrap)
         self._handle_exceptions(popen_exceptions, compilers)
 
     def detect_objcpp_compiler(self, want_cross):
