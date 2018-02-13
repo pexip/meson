8d7811295cc9dfdc98c64c22787b0d1bc67b300f
Jussi Pakkanen <jpakkane@gmail.com>
2018-02-13 19:57:46 +0000

    Convert custom target sources to Files with self.source_strings_to_files.

--- mesonbuild/interpreter.py
+++ mesonbuild/interpreter.py
@@ -3241,6 +3241,8 @@ external dependencies (including libraries) must go to "dependencies".''')
         'Implementation-only, without FeatureNew checks, for internal use'
         name = args[0]
         kwargs['install_mode'] = self._get_kwarg_install_mode(kwargs)
+        if 'input' in kwargs:
+            kwargs['input'] = self.source_strings_to_files(extract_as_list(kwargs, 'input'))
         tg = CustomTargetHolder(build.CustomTarget(name, self.subdir, self.subproject, kwargs), self)
         self.add_target(name, tg.held_object)
         return tg
@@ -3932,7 +3934,7 @@ Try setting b_lundef to false instead.'''.format(self.coredata.base_options['b_s
             sources = [sources]
         for s in sources:
             if isinstance(s, (mesonlib.File, GeneratedListHolder,
-                              CustomTargetHolder, CustomTargetIndexHolder)):
+                              TargetHolder, CustomTargetIndexHolder)):
                 pass
             elif isinstance(s, str):
                 self.validate_within_subproject(self.subdir, s)
