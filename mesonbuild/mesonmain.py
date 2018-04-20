6187190ff9e3cbaad22d252fe8602adc9284e442
Stian Selnes <stian@pexip.com>
2018-04-20 09:38:56 +0200

    [python 3.5->3.4] Revert "Bump minimum supported Python from 3.4 to 3.5."
    This reverts commit 0538009d30c02aba7f3b0e2fb22589c55fbe0fd0.

--- mesonbuild/mesonmain.py
+++ mesonbuild/mesonmain.py
@@ -245,8 +245,8 @@ def set_meson_command(mainfile):
         mlog.log('meson_command is {!r}'.format(mesonlib.meson_command))
 
 def run(original_args, mainfile):
-    if sys.version_info < (3, 5):
-        print('Meson works correctly only with python 3.5+.')
+    if sys.version_info < (3, 4):
+        print('Meson works correctly only with python 3.4+.')
         print('You have python %s.' % sys.version)
         print('Please update your environment')
         return 1
