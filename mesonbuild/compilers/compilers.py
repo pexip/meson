eacb626f7e15c9a5edc95ee940264489910d2145
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-01-16 13:56:43 -0500

    better handle link args: PGI and Flang
    correct flang, pgi options

--- mesonbuild/compilers/compilers.py
+++ mesonbuild/compilers/compilers.py
@@ -1633,7 +1633,10 @@ class PGICompiler:
             return ['-silent']
 
         def openmp_flags(self):
-            return ['-fopenmp']
+            return ['-mp']
+
+    def get_allow_undefined_link_args(self):
+        return []
 
 
 class FlangCompiler:
@@ -1653,7 +1656,10 @@ class FlangCompiler:
             return ['-silent']
 
         def openmp_flags(self):
-            return ['-fopenmp']
+            return ['-mp']
+
+    def get_allow_undefined_link_args(self):
+        return []
 
 
 class ElbrusCompiler(GnuCompiler):
