ea0f9f82865fe9d3af35831e997845d3753a8566
Dylan Baker <dylan@pnwbakers.com>
2019-04-22 15:54:16 -0700

    Don't use len() to test for container emptiness
    I ran the numbers once before (it's in the meson history) but it's
    *much* faster to *not* use len for testing if a container is empty or
    not.

--- mesonbuild/compilers/compilers.py
+++ mesonbuild/compilers/compilers.py
@@ -1520,7 +1520,7 @@ def gnulike_default_include_dirs(compiler, lang):
                 break
             else:
                 paths.append(line[1:])
-    if len(paths) == 0:
+    if not paths:
         mlog.warning('No include directory found parsing "{cmd}" output'.format(cmd=" ".join(cmd)))
     return paths
 
